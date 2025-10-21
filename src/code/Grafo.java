c28c59225d1fc8c55d3601e109f621d762a686b2
Juancho024 <jerp0001@ce.pucmm.edu.do>
2025-10-21 09:14:26 -0800

    mas avances

--- src/code/Grafo.java
+++ src/code/Grafo.java
@@ -25,15 +25,21 @@ public class Grafo {
             Aristas arista = new Aristas(nodoOrigen, nodoDestino, peso);
             rutas.computeIfAbsent(origen, k -> new LinkedList<>()).add(arista);
         } else {
-            System.out.println("Uno de los nodos no existe.");
+            System.out.println("Uno de los lugares no existe.");
         }
     }
 
     public void mostrarGrafo(){
+        System.out.print("Grafo: \n");
         for (String aux : rutas.keySet()) {
-            System.out.print("Nodo " + aux + " -> ");
-            for (Aristas arista : rutas.get(aux)) {
-                System.out.print(arista.getDestino().getNombre() + "(peso: " + arista.getPeso() + ") ");
+            LinkedList<Aristas> listRutas = rutas.get(aux);
+            System.out.print("Lugar " + aux + " tiene rutas al ");
+            for(int i = 0; i < listRutas.size(); i++){
+                Aristas arista = listRutas.get(i);
+                System.out.print(arista.getDestino().getNombre() + " hay " + arista.getPeso() + "km");
+                if(i != listRutas.size() - 1){
+                    System.out.print(", al ");
+                }
             }
             System.out.println();
         }
@@ -47,11 +53,11 @@ public class Grafo {
 
         grafo.agregarArista("A", "B", 5);
         grafo.agregarArista("B", "C", 10);
-        grafo.agregarArista("A", "C", 15);
+        grafo.agregarArista("A", "C", 12);
 
         grafo.mostrarGrafo();
-        grafo.dijkstra("E", "C");
-
+        grafo.dijkstra("A", "C");
+        grafo.floydWarshall();
     }
 
 
@@ -87,12 +93,63 @@ public class Grafo {
             }
             System.out.println("Distancia mínima de " + inicio + " a " + fin + ": " + distancia.get(fin));
         } else {
-            System.out.println("Uno de los nodos no existe.");
+            System.out.println("Uno de los lugares no existe.");
         }
 
     }
     private void floydWarshall() {
         // Implementación del algoritmo de Floyd-Warshall
+        LinkedList<String> nombres = new LinkedList<>(lugar.keySet());
+        HashMap<String, Integer> indice = new HashMap<>();
+
+        int n = lugar.size();
+        for(int i = 0; i < n; i++){
+            indice.put(nombres.get(i), i);
+        }
+        int distancia[][] = new int[n][n];
+        final int numMax = Integer.MAX_VALUE / 2;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if (i == j)
+                    distancia[i][j] = 0;
+                else
+                    distancia[i][j] = numMax;
+            }
+        }
+        for(String lugar: lugar.keySet()){
+            for(Aristas ruta: rutas.get(lugar)){
+                    int i = indice.get(lugar);
+                    int j = indice.get(ruta.getDestino().getNombre());
+                    distancia[i][j] = ruta.getPeso();
+            }
+        }
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(distancia[i][j] > distancia[i][k] + distancia[k][j]){
+                        distancia[i][j] = distancia[i][k] + distancia[k][j];
+                    }
+                }
+            }
+        }
+        System.out.println("\nMatriz de distancias mínimas:");
+        System.out.print("     ");
+        for (String nombre : nombres) {
+            System.out.printf("%5s", nombre);
+        }
+        System.out.println();
+        for(int i = 0; i < n; i++){
+            System.out.printf("%5s", nombres.get(i));
+            for (int j = 0; j < n; j++) {
+                if (distancia[i][j] == numMax)
+                    System.out.printf("%5s", "∞");
+                else
+                    System.out.printf("%5d", distancia[i][j]);
+            }
+            System.out.println();
+        }
+
     }
 
     private void bellmanFord(String inicio) {
